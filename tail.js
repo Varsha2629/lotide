67376cf84c6ff2b4f38a726068db878255c336d0
Varsha Panchal <varsha3051@gmail.com>
2022-01-10 23:41:03 -0500

    tail

---
+++ tail.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if(actual !== expected){
+      console.log(`${String.fromCodePoint(0x1F621)} Assertion Failed: ${actual} !== ${expected}`);
+  }else{
+      console.log(`${String.fromCodePoint(0x1F604)} Assertion Passed: ${actual} === ${expected}`);
+  }
+
+};
+
+const tail = function(arr) {
+  let newArray = [...arr];
+  newArray.splice(0,1);
+  return newArray;
+};
+
+let testArray = [1,2,3,4,5];
+
+assertEqual(tail(testArray)[0], 2);
+assertEqual(tail(testArray).length, 2);
