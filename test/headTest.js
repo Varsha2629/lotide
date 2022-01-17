3218e879f29c0340fb517f3a2fc8cb5c9997afec
Varsha Panchal <varsha3051@gmail.com>
2022-01-17 18:49:53 -0500

    headTest to convert using library (Mocha and Chai)

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,28 @@
-const head = require('../head');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    const input = [1,2,3];
+    const expected = 1;
+    assert(head(input)) === (expected);
+  });
+
+  it("returns '5' for ['5']", () => {
+    const input = ['5'];
+    const expected = '5';
+    assert(head(input)) === (expected);
+  });
+
+  it("returns 'Hello, Lighthouse, Labs' for 'Hello'", () => {
+    const input = ['Hello , Ligthouse, Labs'];
+    const expected = 'Hello';
+    assert(head(input)) === (expected); 
+  });
+
+
+
+});
+
 
 
