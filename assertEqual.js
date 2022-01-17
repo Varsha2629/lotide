a5a4aed77edca19d5479e51609669050b9635e24
Varsha Panchal <varsha3051@gmail.com>
2022-01-17 15:12:02 -0500

    testing start

--- assertEqual.js
+++ assertEqual.js
@@ -9,6 +9,8 @@ const assertEqual = function(actual, expected) {
 };
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual('vish', 'vish');
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+// assertEqual('vish', 'vish');
+
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,16 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if(actual !== expected){
-      console.log(`${String.fromCodePoint(0x1F621)} Assertion Failed: ${actual} !== ${expected}`);
-  }else{
-      console.log(`${String.fromCodePoint(0x1F604)} Assertion Passed: ${actual} === ${expected}`);
-  }
-
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(arr){
-  let head = arr[0];
-  return head;
+  return  arr[0];
 };
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Lighthouse");
+
+module.exports = head;
