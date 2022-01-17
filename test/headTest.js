544a7d6b0f4445be330380adef54be45d82f1000
Varsha Panchal <varsha3051@gmail.com>
2022-01-17 16:11:40 -0500

    tailTest

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,7 @@
 const head = require('../head');
+const assertEqual = require('../assertEqual');
 
-head([5,6,7], 5);
-head(["Hello", "Lighthouse", "Labs"], "Hello");
-head(["Hello", "Lighthouse", "Labs"], "Lighthouse");
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
 
