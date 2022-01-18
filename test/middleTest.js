b0b8cc20160a7a07d0e2964340207f2beba0608d
Varsha Panchal <varsha3051@gmail.com>
2022-01-17 20:44:02 -0500

    test middle number

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,30 @@
-const eqArrays = require('../eqArrays');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
+
 const middle = require('../middle');
 
 
-console.log(middle([1, 2, 3, 4, 5]))
-console.log(middle([1, 2, 3, 4]));
-assertArraysEqual(middle([1, 2, 3, 4]), [2,3]);
+describe("#middle", () => {
+
+  it("returns 3 for [1, 2, 3, 4, 5]", () => {
+    const input = [1, 2, 3, 4, 5];
+    const expected = 3;
+    assert.deepEqual(middle(input), expected);
+  });
+
+  it("returns 2.5 for [1, 2, 3, 4]", () => {
+    const input = [1, 2, 3, 4];
+    const expected = 2;
+    assert.deepEqual(middle(input).length, expected);
+  });
+
+  it("returns [2,3] for [1, 2, 3, 4]", () => {
+    const input = [1, 2, 3, 4];
+    const expected = [2, 3];
+    assert.deepEqual(middle(input), expected);
+  });
+
+});
+
+// console.log(middle([1, 2, 3, 4, 5]))
+// console.log(middle([1, 2, 3, 4]));
+// assertArraysEqual(middle([1, 2, 3, 4]), [2,3]);
