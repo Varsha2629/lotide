593503cb63fdc39b4bd2bf4f7dba2ffafdaf251f
Varsha2629 <varsha3051@gmail.com>
2022-05-03 21:42:44 -0400

    add aVeryBigSum logic

--- aVeryBigSum.js
+++ aVeryBigSum.js
@@ -1,7 +1,10 @@
 function aVeryBigSum(ar) {
   // Write your code here
-  
-
+  let sum = 0;
+  for(let value of ar){
+    sum += value;
+  } 
+  return sum;
 }
 // input: 5
 // 1000000001 1000000002 1000000003 1000000004 1000000005
