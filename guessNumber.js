0b354c58506161681ee009911b45591091acd474
Varsha Panchal <varsha3051@gmail.com>
2022-05-23 01:11:15 -0400

    update guessNumber

--- guessNumber.js
+++ guessNumber.js
@@ -1,35 +1,45 @@
 let prompt = require("prompt-sync")();
 //find random number
 // how the Math.random() work
-// find number 
+// find number
 
-let number = Math.floor(Math.random()*100);
+let number = Math.floor(Math.random() * 100);
 
 let newGuess = [];
 let count = 0;
 let guess = prompt("Guess a number: ");
-let foundCorrectNumber = false;
-while(!foundCorrectNumber){
-  if(number < guess) {   
-    count++;
-    newGuess.push(guess);
-    console.log(number, 'Too Low!');
-  
-  } else if(number > guess) { 
-    count++;
-    newGuess.push(guess);
-    console.log(number, 'Too High!');
- 
-  } else if(newGuess.includes(guess)) {
-    console.log(newGuess);  
-    console.log(newGuess, 'Already Guessed!'); 
-  
-  } else if(!Number(guess)) {
-    console.log(guess, 'Not a number! Try again!'); 
 
-  } else if(guess === number) {
-    foundCorrectNumber = true;
-    console.log(number, `You got it! You took ${count} attempts!`);
-  }
-  break;
+const guessNum = (guess, number, count, newGuess) => {
+console.log(number);
+if (!Number(guess)) {
+  console.log(guess, "Not a number! Try again!");
+  guess = prompt("Guess a number: ");
+  guessNum(guess, number, count, newGuess);
+
+} 
+if (+guess === number) {
+
+  console.log(number, `You got it! You took ${count} attempts!`);
+} else if (newGuess.includes(guess)) {
+  console.log(newGuess, "Already Guessed!");
+  guess = prompt("Guess a number: ");
+  guessNum(guess, number, count, newGuess);
+
+} else if (number < guess) {
+  count++;
+  newGuess.push(guess);
+  console.log(guess, "Too Low!");
+  guess = prompt("Guess a number: ");
+  guessNum(guess, number, count, newGuess);
+  
+} else if (number > guess) {
+  count++;
+  newGuess.push(guess);
+  console.log(guess, "Too High!");
+  guess = prompt("Guess a number: ");
+  guessNum(guess, number, count, newGuess);
+
 }
+}
+
+guessNum(guess, number, count, newGuess);
