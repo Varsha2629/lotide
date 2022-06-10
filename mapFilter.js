bfe6346ac253dfb82e0c0af4d2a6c569c525216c
Varsha Panchal <varsha3051@gmail.com>
2022-06-10 00:07:30 -0400

    filterWithMap

--- mapFilter.js
+++ mapFilter.js
@@ -1,3 +1,13 @@
-
+const users = [
+  { firstName: "Sachin", lastName: "Panchal", age: 20 },
+  { firstName: "Varsha", lastName: "Panchal", age: 70},
+  { firstName: "Bhoomi", lastName: "patel", age: 50 },
+  { firstName: "Isha", lastName: "nagrani", age: 20 },
+];
 // const output = arr.map((x) => x.toString(2));  // for binary value
 // console.log(output);
+
+const output = users.filter((x) => x.age < 30).map((x) => x.firstName);
+console.log(output);
+
+// o/p: ['Sachin', 'Isha']
--- reduce.js
+++ reduce.js
@@ -1,4 +1,10 @@
-const arr = [5, 1, 3, 2, 6];
+// const arr = [5, 1, 3, 2, 6];
+const users = [
+  { firstName: "Sachin", lastName: "Panchal", age: 20 },
+  { firstName: "Varsha", lastName: "Panchal", age: 70},
+  { firstName: "Bhoomi", lastName: "patel", age: 50 },
+  { firstName: "Isha", lastName: "nagrani", age: 20 },
+];
 
 function findSum(arr) {
   let sum = 0;
@@ -12,3 +18,13 @@ const outPut = arr.reduce(function(acc, curr) {
   acc = acc + curr
   return acc;
 }, 0);
+
+const output = users.reduce(function (manup, curr){
+  if (manup[curr.age]) {
+    manup[curr.age] = ++manup[curr.age];
+  } else {
+    manup[curr.age] = 1;
+  }
+  console.log(manup);
+  return manup;
+}, {});
