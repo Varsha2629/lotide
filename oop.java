1075d169f86bd8bc1e39bdece94060b3c942394e
Varsha2629 <varsha3051@gmail.com>
2022-07-19 15:30:52 -0400

    small change on oop

--- oop.java
+++ oop.java
@@ -1,3 +1,14 @@
+// Object
+// Java Object
+// Any entity that has state and behavior is known as an object. For example, a chair, pen, table, keyboard, bike, etc. It can be physical or logical.
+
+// An Object can be defined as an instance of a class. An object contains an address and takes up some space in memory. Objects can communicate without knowing the details of each other's data or code. The only necessary thing is the type of message accepted and the type of response returned by the objects.
+// Example: A dog is an object because it has states like color, name, breed, etc. as well as behaviors like wagging the tail, barking, eating, etc.
+
+// Class
+// Collection of objects is called class. It is a logical entity.
+// A class can also be defined as a blueprint from which you can create an individual object. Class doesn't consume any space.
+
 // Inheritance
     // The idea behind inheritance in Java is that you can create new classes that are built upon existing classes. When you inherit from an existing class, you can reuse methods and fields of the parent class. Moreover, you can add new methods and fields in your current class also.
     // Inheritance represents the IS-A relationship which is also known as a parent-child relationship.
@@ -84,6 +95,7 @@
 
 // Abstraction
     // Abstraction is a process of hiding the implementation details and showing only functionality to the user.
+    // Example: 
     abstract class Bike{  
         abstract void run();  
       }  
